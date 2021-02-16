bd5611b040d3e60cea8880f3d151db3136f170f7
4maumau <mauriciowgarcia@gmail.com>
2021-02-15 23:07:46 -0300

    jump squeeze + collision

---
+++ MuitosBlobs/Assets/Scripts/ChickenAnimator.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChickenAnimator : MonoBehaviour
+{
+    private SpriteRenderer chickenSprite;
+
+    private void Start()
+    {
+        chickenSprite = GetComponent<SpriteRenderer>();
+    }
+
+    private void Update()
+    {
+        FlipSprite();
+    }
+
+    private void FlipSprite()
+    {
+        Vector2 mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        chickenSprite.flipX = mouseWorldPos.x < transform.position.x;
+    }
+
+    public void DoSqueeze(float xSqueeze, float ySqueeze, float seconds)
+    {
+        StartCoroutine(JumpSqueeze(xSqueeze, ySqueeze, seconds));
+    }
+
+    IEnumerator JumpSqueeze(float xSqueeze, float ySqueeze, float seconds)
+    {
+        Vector3 originalSize = Vector3.one;
+        Vector3 newSize = new Vector3(xSqueeze, ySqueeze, originalSize.z);
+        float t = 0f;
+        while (t <= 1.0)
+        {
+            t += Time.deltaTime / seconds;
+            chickenSprite.transform.localScale = Vector3.Lerp(originalSize, newSize, t);
+            yield return null;
+        }
+        t = 0f;
+        while (t <= 1.0)
+        {
+            t += Time.deltaTime / seconds;
+            chickenSprite.transform.localScale = Vector3.Lerp(newSize, originalSize, t);
+            yield return null;
+        }
+
+    }
+}
--- MuitosBlobs/Assets/Scripts/FakeHeight.cs
+++ MuitosBlobs/Assets/Scripts/FakeHeight.cs
@@ -18,18 +18,28 @@ public class FakeHeight : MonoBehaviour
     private float verticalVelocity;
     private Vector2 groundVelocity;
 
+    [Header("Animation")]
     private bool isGrounded;
+    private Animator animator;
+    [SerializeField] private GameObject chickenSprite;
+    private ChickenAnimator chickenAnimator;
+    
 
-    private MousePosition mousePosition;
+    private MousePosition mousePositionScript;
 
-    private Vector2 target;
-    public float maxDistanceDelta = 5;
+   
+    
+
+
 
 
     private void Start()
     {
-        mousePosition = FindObjectOfType<MousePosition>();
-        mousePosition.OnMouseClick.AddListener(MouseClick);
+        mousePositionScript = FindObjectOfType<MousePosition>();
+        mousePositionScript.OnMouseClick.AddListener(MouseClick);
+
+        animator = GetComponentInChildren<Animator>();
+        chickenAnimator = GetComponentInChildren<ChickenAnimator>();
     }
 
     private void Update()
@@ -44,8 +54,7 @@ public class FakeHeight : MonoBehaviour
        
         if (!isGrounded)
         {
-            //transformObject.position += (Vector3)groundVelocity * Time.deltaTime;
-            transformObject.position = Vector2.MoveTowards(transformObject.position, target, maxDistanceDelta * Time.deltaTime);
+            transformObject.position += (Vector3)groundVelocity * Time.deltaTime;
 
             verticalVelocity += gravity * Time.deltaTime;
             transformBody.position += new Vector3(0, verticalVelocity, 0) * Time.deltaTime;
@@ -56,17 +65,17 @@ public class FakeHeight : MonoBehaviour
 
     public void JumpTo(Vector2 _groundVelocity, float _verticalVelocity)
     {
+        chickenAnimator.DoSqueeze(0.3f, 1.6f, 0.1f);
+
         groundVelocity = _groundVelocity; 
         verticalVelocity = _verticalVelocity;
-
-        
     }
 
     
     private void MouseClick(Vector2 mousePos)
     {
         isGrounded = false;
-        
+        //animator.Play("ChickenJump");
 
         Vector2 jumpDirection =  mousePos - (Vector2) transformObject.position;
         float distanceMouseToChicken = Vector2.Distance(mousePos, transformObject.position); // algum jeito de pegar essa distancia e regular com o jumpVelocity
@@ -76,11 +85,7 @@ public class FakeHeight : MonoBehaviour
 
         jumpDirection.Normalize();
 
-
-        //JumpTo(jumpDirection * moveSpeed, jumpVelocity * distanceMouseToChicken);
-        target = mousePos;
-        verticalVelocity = jumpVelocity;
-        
+        JumpTo(jumpDirection * moveSpeed, jumpVelocity);
     }
 
     private void CheckGroundHit()
@@ -88,8 +93,12 @@ public class FakeHeight : MonoBehaviour
         if (transformBody.position.y < transformObject.position.y && !isGrounded)
         {
             isGrounded = true;
-
+            chickenAnimator.DoSqueeze(1.45f, 0.45f, 0.08f);
+            animator.Play("ChickenIdle");
         }
     }
-   
+
+    
+
+    
 }
